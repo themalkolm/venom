c770c688958e7def5f63f59d9e9fe6db0b3b06b0
Alexander Krasnukhin <the.malkolm@gmail.com>
2017-03-03 18:45:25 +0100

    Moar comments

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ even more dangerous!
 
 ## Usage
 
-See `example/` folder for an example how to use venom.
+See `example/` folder for an example how to use venom. You can also use it as a skeletop for
+any 12-factor app you plan to use. It won't solve all problems but it will take care to solve
+the [config](https://12factor.net/config) one.
--- example/src/cmd/example/root.go
+++ example/src/cmd/example/root.go
@@ -11,30 +11,44 @@ import (
 	"github.com/themalkolm/venom"
 )
 
-type Config struct {
-	Foo    string `mapstructure:"foo"`
-	FooBar string `mapstructure:"foo-bar"`
-}
-
 var RootCmd = &cobra.Command{
 	Use:          "example",
 	Short:        "Do example things.",
 	SilenceUsage: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
+		//
+		// 12-factor automatically registered all flags in viper configuration. This
+		// means viper is already configured with all values provided via cli, env
+		// and defaults if any: flag > env > default.
+		//
+		// The following trick with having a struct representing our configuration
+		// allows us to escape dynamic nature of viper and have a static Config
+		// structure.
+		//
 		var cfg Config
 		err := viper.Unmarshal(&cfg)
 		if err != nil {
 			return err
 		}
-
 		return runE(&cfg)
 	},
 }
 
+type Config struct {
+	Foo    string `mapstructure:"foo"`
+	FooBar string `mapstructure:"foo-bar"`
+}
+
 func init() {
+	//
+	// You need to define flags as usual.
+	//
 	RootCmd.PersistentFlags().String("foo", "", "Some foonees must be set")
 	RootCmd.PersistentFlags().String("foo-bar", "", "Some barness must be set")
 
+	//
+	// Enable 12-factor application so magic happens
+	//
 	err := venom.TwelveFactor("example", RootCmd.PersistentFlags())
 	if err != nil {
 		log.Fatal(err)
@@ -42,6 +56,10 @@ func init() {
 }
 
 func runE(cfg *Config) error {
+	//
+	// Here we simply dump passed config object. This has nothing to do
+	// with 12-factor, just make it easy to play with.
+	//
 	b, err := yaml.Marshal(&cfg)
 	if err != nil {
 		return err
