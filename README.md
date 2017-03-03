ad783a3b8a6baeeaadfaa8d9600b20c8f143008e
Alexander Krasnukhin <the.malkolm@gmail.com>
2017-03-03 17:14:11 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # venom
+
+Add some venom to make [cobra](https://github.com/spf13/cobra) and [viper](https://github.com/spf13/viper)
+even more dangerous!
---
+++ venom.go
@@ -0,0 +1,49 @@
+package venom
+
+import (
+	"strings"
+
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+// Teach viper to search FOO_BAR for every --foo-bar key instead of
+// the deafault FOO-BAR.
+func AutomaticEnv(flags *pflag.FlagSet, viperMaybe ...*viper.Viper) {
+	v := viper.GetViper()
+	if len(viperMaybe) != 0 {
+		v = viperMaybe[0]
+	}
+
+	replaceMap := make([]string, 0)
+	flags.VisitAll(func(f *pflag.Flag) {
+		name := strings.ToUpper(f.Name)
+		replaceMap = append(replaceMap, name)
+		replaceMap = append(replaceMap, strings.Replace(name, "-", "_", -1))
+	})
+	v.SetEnvKeyReplacer(strings.NewReplacer(replaceMap...))
+}
+
+func TwelveFactor(name string, flags *pflag.FlagSet, viperMaybe ...*viper.Viper) error {
+	v := viper.GetViper()
+	if len(viperMaybe) != 0 {
+		v = viperMaybe[0]
+	}
+
+	// Bind flags and configuration keys 1-to-1
+	err := v.BindPFlags(flags)
+	if err != nil {
+		return err
+	}
+
+	// Set env prefix
+	v.SetEnvPrefix(strings.ToUpper(name))
+
+	// Set automatic env
+	v.AutomaticEnv()
+
+	// Patch automatic env
+	AutomaticEnv(flags, v)
+
+	return nil
+}
