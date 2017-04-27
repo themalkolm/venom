51cb626add0148d164951e69f94e9965243e9ac4
Alexander Krasnukhin <the.malkolm@gmail.com>
2017-04-27 21:06:18 +0200

    Hide AutomaticEnv

--- venom.go
+++ venom.go
@@ -1,7 +1,6 @@
 package venom
 
 import (
-	"sort"
 	"strings"
 
 	"github.com/spf13/pflag"
@@ -10,41 +9,6 @@ import (
 
 func sanitize(s string) string {
 	return strings.Replace(s, "-", "_", -1)
-}
-
-//
-// Better version of viper.AutomaticEnv that searches FOO_BAR for every --foo-bar key in
-// addition to the default FOO-BAR.
-//
-func AutomaticEnv(flags *pflag.FlagSet, viperMaybe ...*viper.Viper) {
-	v := viper.GetViper()
-	if len(viperMaybe) != 0 {
-		v = viperMaybe[0]
-	}
-
-	replaceMap := make(map[string]string, flags.NFlag())
-	flags.VisitAll(func(f *pflag.Flag) {
-		name := strings.ToUpper(f.Name)
-		replaceMap[name] = sanitize(name)
-	})
-
-	keys := make([]string, 0, len(replaceMap))
-	for k := range replaceMap {
-		keys = append(keys, k)
-	}
-
-	// Reverse sort keys, this is to make sure foo-bar comes before foo. This is to prevent
-	// foo being triggered when foo-bar is given to string replacer.
-	sort.Sort(sort.Reverse(sort.StringSlice(keys)))
-
-	values := make([]string, 0, 2*len(keys))
-	for _, k := range keys {
-		values = append(values, k)
-		values = append(values, replaceMap[k])
-	}
-
-	v.SetEnvKeyReplacer(strings.NewReplacer(values...))
-	v.AutomaticEnv()
 }
 
 //
@@ -68,7 +32,7 @@ func TwelveFactor(name string, flags *pflag.FlagSet, viperMaybe ...*viper.Viper)
 	v.SetEnvPrefix(strings.ToUpper(sanitize(name)))
 
 	// Patch automatic env
-	AutomaticEnv(flags, v)
+	automaticEnv(flags, v)
 
 	return nil
 }
