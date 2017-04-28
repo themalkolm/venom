dea5f56ece31bcc8c73eedc19c3db796440b0c80
Alexander Krasnukhin <the.malkolm@gmail.com>
2017-04-28 17:19:43 +0200

    Correctly handle time parsing

--- decode.go
+++ decode.go
@@ -1,16 +1,39 @@
 package venom
 
 import (
+	"reflect"
+	"time"
+
 	"github.com/mitchellh/mapstructure"
 	"github.com/spf13/viper"
 )
+
+// stringToTimeHookFunc returns a DecodeHookFunc that converts
+// strings to time.Time according to layout.
+func stringToTimeHookFunc(layout string) mapstructure.DecodeHookFunc {
+	return func(f reflect.Type, t reflect.Type, data interface{}) (interface{}, error) {
+		if f.Kind() != reflect.String {
+			return data, nil
+		}
+
+		if t != reflect.TypeOf(time.Time{}) {
+			return data, nil
+		}
+
+		// Convert it by parsing
+		return time.Parse(layout, data.(string))
+	}
+}
 
 func defaultDecoderConfig(output interface{}) *mapstructure.DecoderConfig {
 	return &mapstructure.DecoderConfig{
 		Metadata:         nil,
 		Result:           output,
 		WeaklyTypedInput: true,
-		DecodeHook:       mapstructure.StringToTimeDurationHookFunc(),
+		DecodeHook: mapstructure.ComposeDecodeHookFunc(
+			mapstructure.StringToTimeDurationHookFunc(),
+			stringToTimeHookFunc(time.RFC3339),
+		),
 	}
 }
 
