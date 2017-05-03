dad84a27289da9dd2725b647a983af8979b660d3
Alexander Krasnukhin <the.malkolm@gmail.com>
2017-05-03 18:01:04 +0200

    Moar help

--- multierror.go
+++ multierror.go
@@ -20,8 +20,10 @@ func anyNonNil(errs []error) bool {
 }
 
 //
-// Same as multierror.Append but takes extra care to not create
-// non-nil multierror.Error object with no errors.
+// Same as multierror.Append but takes extra care to not create non-nil multierror.Error object with no errors.n
+//
+// multierror.Append(nil, nil) -> &Error{}
+// AppendErr(nil, nil) -> nil
 //
 func AppendErr(err error, errs ...error) error {
 	ret := multierror.Append(err, errs...)
