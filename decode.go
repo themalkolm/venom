eff40bde6916d17f2afacda077f1f7cba171e5cf
Alexander Krasnukhin <the.malkolm@gmail.com>
2017-05-10 01:51:35 +0200

    Rename unmarshal -> decode

--- decode.go
+++ decode.go
@@ -1,6 +1,9 @@
 package venom
 
 import (
+	"time"
+
+	"github.com/mitchellh/mapstructure"
 	"github.com/spf13/viper"
 )
 
@@ -20,4 +23,35 @@ func Decode(out Config, cfg *viper.Viper) error {
 		return err
 	}
 	return out.Valid()
+}
+
+//
+// Same as viper.Unmarshal but with support for string slices, time etc.
+//
+func Unmarshal(out interface{}, cfg *viper.Viper) error {
+	return decode(cfg.AllSettings(), defaultDecoderConfig(out))
+}
+
+func defaultDecoderConfig(output interface{}) *mapstructure.DecoderConfig {
+	return &mapstructure.DecoderConfig{
+		Metadata:         nil,
+		Result:           output,
+		WeaklyTypedInput: true,
+		DecodeHook: mapstructure.ComposeDecodeHookFunc(
+			stringToStringSliceHookFunc(","),
+			stringToBoolSliceHookFunc(","),
+			stringToIntSliceHookFunc(","),
+			stringToUintSliceHookFunc(","),
+			stringToTimeDurationHookFunc(),
+			stringToTimeHookFunc(time.RFC3339),
+		),
+	}
+}
+
+func decode(input interface{}, config *mapstructure.DecoderConfig) error {
+	decoder, err := mapstructure.NewDecoder(config)
+	if err != nil {
+		return err
+	}
+	return decoder.Decode(input)
 }
